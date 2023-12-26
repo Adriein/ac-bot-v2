743e9c7e321b17ae36b89c4835c8693290dd89a1
Adriein <adria.claret@gmail.com>
2023-12-26 11:00:09 +0100

    trying to extract text with tesseract

--- src/TaskPackage/GameContext/Battle/ExtractBattleListDataTask.py
+++ src/TaskPackage/GameContext/Battle/ExtractBattleListDataTask.py
@@ -17,6 +17,8 @@ class ExtractBattleListDataTask(Task):
     def execute(self, context: GameContext, frame: np.ndarray) -> GameContext:
         widget = self.__container.battle_list_widget()
 
+        print(widget)
+
         battle_list_roi = frame[widget.start_y: widget.end_y, widget.start_x: widget.end_x]
 
         battle_list_rgb = cv2.cvtColor(battle_list_roi, cv2.COLOR_BGR2RGB)
