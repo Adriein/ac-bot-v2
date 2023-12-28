386d39779250c8c6c80aa1f306b9fd96753f12ee
Adriein <adria.claret@gmail.com>
2023-12-28 12:25:07 +0100

    ocr for variable text

--- src/VendorPackage/Tesseract/TesseractOcr.py
+++ src/VendorPackage/Tesseract/TesseractOcr.py
@@ -16,25 +16,11 @@ class TesseractOcr:
         rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         grey_img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
+        # Apply thresholding to separate the digits from the background
+        _, thresholded = cv2.threshold(grey_img, 127, 255, cv2.THRESH_BINARY_INV)
 
-        # Apply thresholding to separate the number from the background
-        _, thresholded = cv2.threshold(grey_img, 127, 255, cv2.THRESH_BINARY)
-
-        # Find contours in the thresholded image
-        contours, _ = cv2.findContours(thresholded, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-        # Find the contour with the largest area (assuming the number is the largest object)
-        largest_contour = max(contours, key=cv2.contourArea)
-
-        # Create a mask for the largest contour
-        mask = np.zeros_like(thresholded)
-        cv2.drawContours(mask, [largest_contour], 0, 255, thickness=cv2.FILLED)
-
-        # Bitwise AND operation to get the region of interest (ROI)
-        result = cv2.bitwise_and(frame, frame, mask=mask)
-
-        # Display the result
-        cv2.imshow('Result', result)
+        # Display the thresholded image
+        cv2.imshow('Thresholded Image', thresholded)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
 
