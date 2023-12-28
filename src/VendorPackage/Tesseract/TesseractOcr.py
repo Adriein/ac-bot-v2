c48d799ea2214f29ba1806b59a3cc14c7c24c02a
Adriein <adria.claret@gmail.com>
2023-12-28 12:09:30 +0100

    added missing deps

--- src/VendorPackage/Tesseract/TesseractOcr.py
+++ src/VendorPackage/Tesseract/TesseractOcr.py
@@ -3,6 +3,8 @@ import cv2
 import pytesseract
 import re
 
+from pytesseract import Output
+
 
 class TesseractOcr:
     TESSERACT_EXTRACT_NUMBER_CONFIG = r'--oem 3 --psm 8 outputbase digits'
@@ -14,7 +16,7 @@ class TesseractOcr:
         rgb_img = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         grey_img = cv2.cvtColor(frame, cv2.COLOR_RGB2GRAY)
 
-        result = pytesseract.image_to_string(grey_img, config=self.TESSERACT_EXTRACT_NUMBER_CONFIG)
+        result = pytesseract.image_to_data(grey_img, config=self.TESSERACT_EXTRACT_NUMBER_CONFIG, output_type=Output.DICT)
 
         print(result)
 
