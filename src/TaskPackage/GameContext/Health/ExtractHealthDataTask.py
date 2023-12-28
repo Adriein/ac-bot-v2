a53e275aa9a0b454ce653f4b165fb2bc1ba79274
Adriein <adria.claret@gmail.com>
2023-12-28 10:32:06 +0100

    trying to extract health from hp

--- src/TaskPackage/GameContext/Health/ExtractHealthDataTask.py
+++ src/TaskPackage/GameContext/Health/ExtractHealthDataTask.py
@@ -23,12 +23,13 @@ class ExtractHealthDataTask(Task):
     def execute(self, context: GameContext, frame: np.ndarray) -> GameContext:
         widget = self.__widget.health_widget()
 
-        hp_roi = frame[widget.start_y: widget.end_y, widget.start_x: widget.end_x + 150]
+        hp_roi = frame[widget.start_y: widget.end_y, widget.start_x + 100: widget.end_x + 150]
         grey_hp_roi = cv2.cvtColor(hp_roi, cv2.COLOR_BGR2GRAY)
         h, w, c = hp_roi.shape
         custom_config = r'--oem 3 --psm 8 outputbase digits'
         img = cv2.cvtColor(hp_roi, cv2.COLOR_BGR2RGB)
         im_pil = Image.fromarray(img)
+        PyAutoGui.debug_image(img)
         print(pytesseract.image_to_string(im_pil, config=custom_config))
         self.success()
         return context
