6b5659db3c5c46c4ffa7d479a883518b425241e5
Adriein <adria.claret@gmail.com>
2024-01-13 09:54:13 +0100

    debugging precision attack

--- src/Cavebot/CaveBot.py
+++ src/Cavebot/CaveBot.py
@@ -2,7 +2,7 @@ from src.GamePackage import Player, Script
 from src.OperatingSystemPackage import Monitor, GlobalGameWidgetContainer, Keyboard
 from src.SharedPackage import GameContext
 from src.TaskPackage import TaskResolver, ExtractGameContextDataTask, AttackTask, HealingTask
-from src.VendorPackage import TesseractOcr
+from src.VendorPackage import TesseractOcr, PyAutoGui
 
 
 class CaveBot:
@@ -23,12 +23,13 @@ class CaveBot:
 
     def start(self, game_context: GameContext, player: Player) -> None:
         screenshot = self.__monitor.screenshot()
-
+        PyAutoGui.debug_image(screenshot)
+        print(screenshot.shape)
         # 1. extract game context
         extract_game_context_data_task = ExtractGameContextDataTask(self.__resolver, self.__widget, self.__tesseract)
-        self.__resolver.queue(extract_game_context_data_task)
+        # self.__resolver.queue(extract_game_context_data_task)
 
-        self.__resolver.resolve(game_context, screenshot)
+        # self.__resolver.resolve(game_context, screenshot)
 
         # 2. auto healing
         healing_task = HealingTask(player)
@@ -36,7 +37,7 @@ class CaveBot:
 
         # 3. auto attacking
         attack_task = AttackTask(self.__resolver, player)
-        self.__resolver.queue(attack_task)
+        # self.__resolver.queue(attack_task)
 
         # 4. auto looting
 
