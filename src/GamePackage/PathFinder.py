76a587fa8d63f1eba1b6a88d1df11a39eaad88a0
Adriein <adria.claret@gmail.com>
2024-02-17 19:44:46 +0100

    fixed pathfinding

--- src/GamePackage/PathFinder.py
+++ src/GamePackage/PathFinder.py
@@ -53,7 +53,11 @@ class PathFinder:
         hashed_route = self.__hash_waypoint_set(current, destination)
 
         if self.RESOLVED_PATHS_CACHE.has(hashed_route):
-            return self.RESOLVED_PATHS_CACHE.get(hashed_route)
+            path: LinkedList[MoveCommand] = self.RESOLVED_PATHS_CACHE.get(hashed_route)
+
+            path.reset()
+
+            return path
 
         path = LinkedList()
 
