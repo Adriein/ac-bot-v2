c702a73e47f02002a88b3cc70f990b7bd96f60b1
Adriein <adria.claret@gmail.com>
2024-03-02 10:57:50 +0100

    added chase opponent feature

--- src/TaskPackage/GameActions/WalkTask.py
+++ src/TaskPackage/GameActions/WalkTask.py
@@ -33,7 +33,8 @@ class WalkTask(Task):
         real_current_position = context.get_current_waypoint()
 
         walk_instructions = self.__game_map.find_shortest_path(real_current_position, destination)
-
+        print(walk_instructions)
+        print(destination)
         for instruction in walk_instructions:
             self.__player.move(instruction)
             time.sleep(0.4)
