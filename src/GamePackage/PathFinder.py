2ed75952c7115996e305a1f65eb690b193416bf5
Adriein <adria.claret@gmail.com>
2024-03-03 12:05:06 +0100

    changing ocr to normal parsing

--- src/GamePackage/PathFinder.py
+++ src/GamePackage/PathFinder.py
@@ -136,9 +136,10 @@ class PathFinder:
             x, y, z = false_positive.split(',')
 
             waypoint = Waypoint(x, y, z, "false")
-            print('EVALUATE EQUALITY')
-            print(waypoint)
-            print(current.waypoint)
+            if current.waypoint.x == 32435 and current.waypoint.y == 32295:
+                print('EVALUATE EQUALITY')
+                print(waypoint)
+                print(current.waypoint)
             if waypoint == current.waypoint:
                 return False
 
