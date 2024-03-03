a077727730e22ea889e05de32d2a170bcbee1775
Adriein <adria.claret@gmail.com>
2024-03-03 08:30:29 +0100

    changing ocr to normal parsing

--- src/TaskPackage/GameContext/ExtractGameContextDataTask.py
+++ src/TaskPackage/GameContext/ExtractGameContextDataTask.py
@@ -44,7 +44,7 @@ class ExtractGameContextDataTask(Task):
 
         Logger.debug("Queueing ExtractHealthDataTask")
         extract_health_data_task = ExtractHealthDataTask(self.__widget, self.__pyautogui)
-        self.__resolver.queue(extract_health_data_task)
+        #self.__resolver.queue(extract_health_data_task)
 
         Logger.debug("Queueing ExtractManaDataTask")
         extract_mana_data_task = ExtractManaDataTask(self.__widget, self.__tesseract)
