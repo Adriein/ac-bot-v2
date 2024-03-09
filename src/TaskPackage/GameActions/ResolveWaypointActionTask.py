0b6b5c2aac7ac2b77b8a05d38dd04381826b0549
Adriein <adria.claret@gmail.com>
2024-03-09 12:23:24 +0100

    parsing waypoints of false positives

--- src/TaskPackage/GameActions/ResolveWaypointActionTask.py
+++ src/TaskPackage/GameActions/ResolveWaypointActionTask.py
@@ -40,6 +40,11 @@ class ResolveWaypointActionTask(Task):
         print('----------------------------------------')
         print(current_waypoint)
 
+        if current_waypoint.x == 32430 and current_waypoint.y == 32302:
+            print(current_waypoint.type)
+            print(current_waypoint.type is Waypoint.HOLE_UP_TYPE)
+            raise Exception
+
         game_window = self.__widget.game_window()
         if current_waypoint.is_floor_change_type():
             if current_waypoint.type is Waypoint.HOLE_UP_TYPE:
