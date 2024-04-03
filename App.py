f39dec3c99130093d21ee27b151f82ef893c4633
Adriein <adria.claret@gmail.com>
2024-04-03 08:29:37 +0200

    change script

--- App.py
+++ App.py
@@ -93,8 +93,8 @@ class TibiaAcBot:
         self.__mouse = Mouse(self.__kernel)
 
         Logger.info('Loading Script...')
-        # self.__script = Script.load('src/Wiki/Script/Venore/swampling_cave_floor_10.json')
-        self.__script = Script.load('src/Wiki/Script/Thais/thais_wasp.json')
+        self.__script = Script.load('src/Wiki/Script/Venore/swampling_cave_floor_10.json')
+        # self.__script = Script.load('src/Wiki/Script/Thais/thais_wasp.json')
 
         initial_floor_lvl = list(self.__script.floors())[0]
 
