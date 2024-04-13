48bf5d710d671bf2e47330deea2f3b5e90bdfa94
Adriein <adria.claret@gmail.com>
2024-04-13 12:45:54 +0200

    trying to fix how we up or down ramps

--- src/TaskPackage/GameActions/WalkTask.py
+++ src/TaskPackage/GameActions/WalkTask.py
@@ -32,7 +32,7 @@ class WalkTask(Task):
 
         route = context.get_cave_route()
         destination = route.current.data
-
+        print(destination)
         real_current_position = context.get_current_waypoint()
 
         walk_instructions = self.__game_map.find_shortest_path(real_current_position, destination)
