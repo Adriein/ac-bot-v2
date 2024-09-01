8cd50e287bc63a8bfd3ea1f36a8691d55fcf3a97
Adriein <adria.claret@gmail.com>
2024-09-01 12:58:51 +0200

    feat: trying to parse info from item

--- src/TaskPackage/GameContext/Market/ExtractSelectedItemInfo.py
+++ src/TaskPackage/GameContext/Market/ExtractSelectedItemInfo.py
@@ -41,11 +41,10 @@ class ExtractSelectedItemInfo(Task):
                 height = region.end_y - region.start_y
                 end_y = region.end_y + height
 
-                print(region)
-                amount_roi = grey_frame[region.start_y:end_y, region.start_x:region.end_x]
+                amount_roi = grey_frame[start_y:end_y, region.start_x:region.end_x]
                 # amount_roi = grey_frame[region.start_y:region.end_y, region.start_x:region.end_x]
 
-                PyAutoGui.debug_image(grey_frame)
+                PyAutoGui.debug_image(amount_roi)
 
                 raise KeyboardInterrupt
 
