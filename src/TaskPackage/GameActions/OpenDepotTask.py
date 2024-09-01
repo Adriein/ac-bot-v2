0dd455b03b42ff18a02dee1b8d4f5c6b9a7413b8
Adriein <adria.claret@gmail.com>
2024-09-01 10:18:03 +0200

    feat: trying to open market

--- src/TaskPackage/GameActions/OpenDepotTask.py
+++ src/TaskPackage/GameActions/OpenDepotTask.py
@@ -1,3 +1,5 @@
+from time import sleep
+
 import numpy as np
 import cv2
 
@@ -31,6 +33,8 @@ class OpenDepotTask(Task):
 
             Logger.debug("Updated context")
             Logger.debug(context, inspect_class=True)
+
+            sleep(5)
 
             self.success()
             return context
